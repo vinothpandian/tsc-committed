3c2e4784c2de56b2151597ce07195606a0a9d887
Vinoth Pandian <vinothpandian@users.noreply.github.com>
2023-03-11 22:33:54 -0500

    fix: git doesn't exist or not a git repo

--- src/commands/git.ts
+++ src/commands/git.ts
@@ -1,6 +1,7 @@
 import { execa, ExecaError } from "execa";
 import { compact, split } from "lodash-es";
 import { logError, logInfo } from "../utils";
+import { stopSpinner } from "../utils/spinner";
 
 export async function getModifiedFilesListFromGit(
   baseBranch: string,
@@ -21,8 +22,35 @@ export async function getModifiedFilesListFromGit(
 
     return compact(split(stdout, "\n"));
   } catch (e) {
-    const error = e as ExecaError;
-    logError(error.shortMessage);
+    stopSpinner();
+    const { stderr, shortMessage } = e as ExecaError;
+
+    console.log(stderr);
+
+    // Verify git is installed
+    if (stderr.includes("git: command not found")) {
+      stopSpinner();
+      logError("Git is not installed. Please install git.");
+      process.exit(1);
+    }
+
+    // Verify if it is a git repository
+    if (stderr.includes("Not a git repository")) {
+      stopSpinner();
+      logError(
+        "Not a git repository. Please run the command in a git repository."
+      );
+      process.exit(1);
+    }
+
+    // Verify base branch exists
+    if (stderr.includes("fatal: ambiguous argument")) {
+      stopSpinner();
+      logError(`Base branch ${baseBranch} not found. Check your git config.`);
+      process.exit(1);
+    }
+
+    logError(shortMessage);
     logInfo("Check if you are in the correct directory");
   }
 
